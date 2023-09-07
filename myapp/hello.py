f51ba4a589390448cc8e959c0f1fbe5c89f00dcd
DevShalev <94471389+DevShalev@users.noreply.github.com>
2023-09-07 20:41:07 +0300

    Update hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -4,7 +4,7 @@ def hello(name="World"):
   return "Hello %s!" % name
 
 def taxcal(number=1000):
-  return "tax 17'%' of x is: %d!" % float(number)*0.17
+  return "Tax 17'%' of %d is: %.2f!" % (number, float(number) * 0.17)
 
 if __name__ == '__main__':
   fire.Fire(hello)
